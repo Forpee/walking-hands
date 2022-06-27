39b636336e3789c26be0aa115b5b1f82d087f3a0
Forpee <khalilhassam3@gmail.com>
2022-06-27 19:34:08 +0200

    a

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -4,8 +4,8 @@ void main()
 {
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
-
-    gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
+    
+    gl_Position=projectionMatrix*modelViewMatrix*instanceMatrix*vec4(position,1.);
     
     vUv=uv;
 }
