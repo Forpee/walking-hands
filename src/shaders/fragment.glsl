1e1dd73b6f16742822fa85d933ba42164cb6afac
Forpee <khalilhassam3@gmail.com>
2022-06-27 19:41:57 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -5,12 +5,14 @@ varying vec2 vUv;
 
 void main()
 {
-    vec4 color=texture2D(uSprite,vUv);
     
     vec2 offset=vec2(
         mod(floor(uTime),5.),
         4.-floor(mod(uTime/5.,5.))
     );
+    vec2 uv1=vUv/5.+offset/5.;
+    
+    vec4 color=texture2D(uSprite,uv1);
     
     gl_FragColor=vec4(vUv,1.,1.);
     gl_FragColor=color;
